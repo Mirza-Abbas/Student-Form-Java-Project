5dc23d4bd95876fca6e74fa687bb37c468864d7b
Mirza Muhammad Abbas <abbasmeerza786@gmail.com>
2022-09-28 20:14:30 +0500

    Added address & country option

--- src/StudentFormDemo.java
+++ src/StudentFormDemo.java
@@ -1,11 +1,18 @@
 import org.json.simple.JsonObject; 
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
 import javax.swing.JTextField;
 import javax.swing.JRadioButton;
+import javax.swing.JTextArea;
 import javax.swing.ButtonGroup;
+import javax.swing.ImageIcon;
+
+import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -14,9 +21,11 @@ import java.io.Writer;
 
 class StudentForm extends JFrame implements ActionListener{
 
+    JsonObject Object1;
+
     JTextField Name, RollNO, Batch, Section;
 
-    JLabel LabelName, LabelRollNo, LabelBatch, LabelSection, LabelGender;
+    JLabel LabelName, LabelRollNo, LabelBatch, LabelSection, LabelGender, LabelQualification, LabelAddress, LabelCountry;
 
     JButton SaveButton, PrintButton;
 
@@ -24,14 +33,22 @@ class StudentForm extends JFrame implements ActionListener{
 
     JOptionPane MessagePanel;
 
-    ButtonGroup Group1;  // for Male & Female Button
+    JCheckBox Matric, Intermediate, Graduate, PostGraduate;
+
+    JTextArea Address;
+
+    String[] Countries={"Pakistan", "India", "China", "Iran", "Saudi Arab"};
+    JComboBox Country;
+    
+    ButtonGroup GenderGroup;  // for Male & Female Button
 
     public StudentForm(){   //Constructor
 
-        setSize(300, 300);
+        setSize(350, 350);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new FlowLayout(FlowLayout.LEFT));
-        
+        setTitle("Student Registration Form");
+        getContentPane().setBackground(Color.cyan);
 
         RollNO= new JTextField(25);
         Name = new JTextField(25);
@@ -43,6 +60,9 @@ class StudentForm extends JFrame implements ActionListener{
         LabelBatch = new JLabel("Batch: ");
         LabelSection = new JLabel("Section: ");
         LabelGender = new JLabel("Gender:");
+        LabelQualification=new JLabel("Qualification:");
+        LabelAddress =new JLabel("Address:");
+        LabelCountry=new JLabel("Country:");
 
         SaveButton = new JButton("Save");
         PrintButton = new JButton("Print");
@@ -50,9 +70,18 @@ class StudentForm extends JFrame implements ActionListener{
         MaleButton=new JRadioButton("Male");
         FemaleButton=new JRadioButton("Female");
 
+        Matric=new JCheckBox("Matric");
+        Intermediate=new JCheckBox("Intermediate");
+        Graduate=new JCheckBox("Graduate");
+        PostGraduate=new JCheckBox("PostGraduate");
+
+        Address=new JTextArea(6, 15);
+
+        Country=new JComboBox(Countries);
+
         MessagePanel=new JOptionPane();
 
-        Group1 = new ButtonGroup();
+        GenderGroup = new ButtonGroup();
 
         add(LabelName);
         add(Name);
@@ -65,101 +94,152 @@ class StudentForm extends JFrame implements ActionListener{
         add(LabelGender);
         add(MaleButton);
         add(FemaleButton);
+        add(LabelQualification);
+        add(Matric);
+        add(Intermediate);
+        add(Graduate);
+        add(PostGraduate);
+        add(LabelAddress);
+        add(Address);
+        add(LabelCountry);
+        add(Country);
         add(SaveButton);
         add(PrintButton);
 
-        Group1.add(MaleButton);      
-        Group1.add(FemaleButton);
+        GenderGroup.add(MaleButton);      
+        GenderGroup.add(FemaleButton);
 
         SaveButton.addActionListener(this);
         PrintButton.addActionListener(this);
         setVisible(true);
     }
 
+    void save(){ // method for when save button is selected
+        Writer write;
+        try {
+            write = new FileWriter("Task.json");
+                write.write(Object1.toString());
+                write.close();
+        } catch (Exception a) {
+            a.printStackTrace();
+        }
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
         Writer writer;  // For JSON
-        JsonObject Object1 = new JsonObject();
+        Object1 = new JsonObject();
 
-        Object1.put("Name", Name.getText());
-        Object1.put("RollNo: ", RollNO.getText());
-        Object1.put("Batch: ", Batch.getText());
-        Object1.put("Section: ", Section.getText());
+        Object1.put("Name ", Name.getText());
+        Object1.put("RollNo ", RollNO.getText());
+        Object1.put("Batch ", Batch.getText());
+        Object1.put("Section ", Section.getText());
+        Object1.put("Address ", Address.getText());
+        Object1.put("Country ", Countries[Country.getSelectedIndex()]);
 
-        if(MaleButton.isSelected()) {       // When Male Button's Selected
-            Object1.put("Gender: ", MaleButton.getText());
-        } else {                            //else Female Button's Selected
-            Object1.put("Gender: ", FemaleButton.getText());
+        { //Gender Selection
+            if(MaleButton.isSelected()) {       // When Male Button's Selected
+                Object1.put("Gender ", MaleButton.getText());
+            } else {                            //else Female Button's Selected
+                Object1.put("Gender ", FemaleButton.getText());
+            }
+        }
+
+        { //Qualification Selection
+            if(Matric.isSelected()){    //When Matric is Selected
+                Object1.put("Qualification ", Matric.getText());
+            }
+            if(Intermediate.isSelected()){        //When Intermediate is Selected                    
+                Object1.put("Qualification ", Intermediate.getText());
+            }
+            if(Graduate.isSelected()){      //When Graduate is Selected
+                Object1.put("Qualification ", Graduate.getText());
+            }
+            if(PostGraduate.isSelected()){     //When PostGraduate is Selected                       
+                Object1.put("Qualification ", PostGraduate.getText());
+            }
         }
 
         if(e.getSource()==SaveButton) {     // when save button is selected
             try {
-                writer = new FileWriter("Task.json");
-                writer.write(Object1.toString());
-                try{
-                    if(MaleButton.isSelected()) {
-                        Object1.put("Gender: ", MaleButton.getText());
-                    }
-                    else {
-                        Object1.put("Gender: ", FemaleButton.getText());
-                    }
-                }
-                catch (Exception i){
-                    i.printStackTrace();
-                }
-
-                writer.close();
-                
+                save();  //to write in JSON File
                 MessagePanel.showMessageDialog(null, "Details Saved");
                 //shows message that details are saved 
             } catch (Exception a) {
                 a.printStackTrace();
             }
-
         }
         else if(e.getSource()==PrintButton) {   // when Print button is selected
-            
-            this.dispose();
+
+            save();     //method to write in JSON File
+            this.dispose();        //Closes First Frame
 
             JFrame F2=new JFrame();     // will open new frame to display details
-            F2.setSize(300,300);
-            F2.setVisible(true);
-            F2.setLayout(new FlowLayout(FlowLayout.LEFT));
-            
-            
+            JPanel P2=new JPanel(null);  //New Panel
 
-            F2.add(LabelName);
+            F2.setLayout(null);     //sets Frame Layout to null
+            F2.setBounds(0, 0, 500, 300);  //sets Frame size
+            F2.setVisible(true);  //sets Frame Visibility
+            F2.setTitle("Student Registration Form");   //sets Frame Title
+            P2.setBackground(Color.CYAN);  //sets Frame color
+
+		    LabelName.setBounds(0, 0, 50, 20);  
+		    P2.add(LabelName);
             LabelName = new JLabel(Name.getText());
-           
-            F2.add(LabelName);
-            F2.add(LabelRollNo);
-            LabelRollNo = new JLabel(RollNO.getText());
-            F2.add(LabelRollNo);
-            F2.add(LabelBatch);
-            LabelBatch = new JLabel(Batch.getText());
-            F2.add(LabelBatch);
-            F2.add(LabelSection);
-            LabelSection = new JLabel(Section.getText());
-            F2.add(LabelSection);
+		    LabelName.setBounds(50, 0, 500, 20);
+            P2.add(LabelName);
+		    F2.setContentPane(P2);
 
-            F2.add(LabelGender);
-            if(MaleButton.isSelected()){
-                LabelGender=new JLabel("Male");
-            }
-            else{
-                LabelGender=new JLabel("Female");
-            }
-            F2.add(LabelGender);
+            LabelRollNo.setBounds(0, 30, 50, 20);
+            P2.add(LabelRollNo);
+            LabelRollNo = new JLabel(RollNO.getText());
+            LabelRollNo.setBounds(50, 30, 500, 20);
+            P2.add(LabelRollNo);
+
+            LabelBatch.setBounds(0, 60, 50, 20);
+            P2.add(LabelBatch);
+            LabelBatch = new JLabel(Batch.getText());
+            LabelBatch.setBounds(50, 60, 500, 20);
+            P2.add(LabelBatch);
+
+            LabelSection.setBounds(0, 90, 50, 20);
+            P2.add(LabelSection);
+            LabelSection = new JLabel(Section.getText());
+            LabelSection.setBounds(50, 90, 500, 20);
+            P2.add(LabelSection);
+
+            LabelGender.setBounds(00, 120, 50, 20);
+            P2.add(LabelGender);
+            LabelGender=new JLabel(Object1.getString("Gender "));
+            LabelGender.setBounds(50, 120, 500, 20);
+            P2.add(LabelGender);
+
+            LabelQualification.setBounds(00, 150, 80, 20);
+            P2.add(LabelQualification);
+            LabelQualification=new JLabel(Object1.getString("Qualification "));
+            LabelQualification.setBounds(80, 150, 500, 20);
+            P2.add(LabelQualification);
+
+            LabelAddress.setBounds(00, 180, 60, 20);
+            P2.add(LabelAddress);
+            LabelAddress=new JLabel(Address.getText());
+            LabelAddress.setBounds(55, 180, 500, 20);
+            P2.add(LabelAddress);
+
+            LabelCountry.setBounds(00, 210, 50, 20);
+            P2.add(LabelCountry);
+            LabelCountry=new JLabel(Countries[Country.getSelectedIndex()]);
+            LabelCountry.setBounds(50, 210, 500, 20);
+            P2.add(LabelCountry);
+
             F2.setDefaultCloseOperation(EXIT_ON_CLOSE);
         }
         
     }
 }
 
-class StudentFormDemo {
+public class StudentFormDemo {
     public static void main(String[] args) {
-
         StudentForm Form1 = new StudentForm();
-
     }
 }
